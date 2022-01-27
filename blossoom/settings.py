86e749c5bf257e2fdb6d48e88bcecee547d3569b
malek salem <malek.salem.14@gmail.com>
2022-01-27 01:52:50 +0100

    testing

--- blossoom/settings.py
+++ blossoom/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
