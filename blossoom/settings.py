58f96bdfb73011cb79d08d4400163060d318d7c5
malek salem <malek.salem.14@gmail.com>
2022-01-27 11:47:12 +0100

    fixing cors headers

--- blossoom/settings.py
+++ blossoom/settings.py
@@ -60,9 +60,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -202,7 +202,6 @@ SIMPLE_JWT = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = False
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
